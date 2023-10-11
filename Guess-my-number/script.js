bb1d21097971d636b90c93e16c79e973d3e8df8a
Castro28 <waltercstr28@gmail.com>
2023-10-11 15:59:08 -0700

    worked on display message

--- Guess-my-number/script.js
+++ Guess-my-number/script.js
@@ -15,14 +15,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '❌No number!';
-
+    // document.querySelector('.message').textContent = '❌No number!';
+    displayMessage('❌No number!');
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number';
